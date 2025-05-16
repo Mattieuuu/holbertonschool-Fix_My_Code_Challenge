e0d2dc7021d5eed43a72cfa66cdd5d6b8340e802
Mattieuuu <mattieu.mx@gmail.com>
2025-05-16 15:03:18 +0200

    push

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -43,11 +43,12 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		(*head)->prev->prev = (*head)->prev;
-		free(*head);
+		tmp = *head;
+		(*head)->prev->next = (*head)->next;
 		if ((*head)->next)
 			(*head)->next->prev = (*head)->prev;
 		*head = saved_head;
+		free(tmp);
 	}
 	return (1);
 }
